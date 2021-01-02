789e08037d23aed3614d195c48bb0bf8be1d16bb
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-01 21:07:29 -0500

    Updated seed files and migration scripts

--- seeds/seed.folders.sql
+++ seeds/seed.folders.sql
@@ -1,5 +1,5 @@
 INSERT INTO folders (id, title)
 VALUES
-    ('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1','Important'),
-    ('b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Super'),
-    ('b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1', 'Spanley')
+    ('1','Important'),
+    ('2', 'Super'),
+    ('3', 'Spanley')

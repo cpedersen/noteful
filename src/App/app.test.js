df12319d904105af03619fdaa43d8140e4c78f0f
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-14 19:00:26 -0500

    Updated test (fails for now)

--- src/App/app.test.js
+++ src/App/app.test.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
+/*test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});*/
+
+/*test('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+});*/
 
 /*test('renders learn react link', () => {
   const { getByText } = render(<App />);

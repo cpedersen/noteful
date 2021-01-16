87343ae881a564308f41350162d0137517435324
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-15 20:46:21 -0500

    Add logging to app.js error handler

--- src/App/app.test.js
+++ src/App/app.test.js
@@ -3,24 +3,21 @@ import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
+
+/*test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
+});*/
 
 /*test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });*/
 
-/*test('sums numbers', () => {
-  expect(sum(1, 2)).toEqual(3);
-  expect(sum(2, 2)).toEqual(4);
-});*/
 
-/*test('renders learn react link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});*/
+});

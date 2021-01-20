273c55957c626b1567ce39599676ed6b5b437107
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-20 15:13:24 -0500

    Added note count to folder titles

--- src/Sidebar/sidebar.js
+++ src/Sidebar/sidebar.js
@@ -7,20 +7,23 @@ class Sidebar extends Component {
     static contextType = NotesContext
     render() {
         let notesContext = this.context
+        const {notes} = notesContext
         const selectedFolder = parseInt(this.props.match.params.folder_id) || {}
-        console.log({selectedFolder})
+        console.log({selectedFolder, notes: notesContext.notes})
         return (
             <section className="Section_Sidebar">
                 <div>
                     {notesContext.folders.map(folder => {
                         console.log(folder)
+                        const folderNotes = notes.filter(({ folder_id }) => folder_id === folder.id)
                         return(
                             <section key={folder.id} className={(selectedFolder && selectedFolder === folder.id)? "Folder SelectedFolder" : "Folder"}>
                                 <NavLink className="Folder_Link" 
                                     tag='button'
                                     role='link'
                                     to={'/api/folder/' + folder.id}>
-                                    {folder.title}
+                                    {folder.title}{" "}
+                                    <small>({folderNotes.length || '-'})</small>
                                 </NavLink>
                             </section>
                         )

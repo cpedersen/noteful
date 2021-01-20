f4c2db815a0b80e89c6d7cc714a1518aa5886f72
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-19 20:02:54 -0500

    Removed commented out code from app.test.js

--- src/App/app.test.js
+++ src/App/app.test.js
@@ -12,14 +12,3 @@ test('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-/*test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});*/
-
-
-/*test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});*/

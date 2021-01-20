74cc4a7940d0e42458b35017047e874723658a27
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-19 19:56:20 -0500

    Fixed test; changed Heroku url

--- src/App/app.test.js
+++ src/App/app.test.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from "react-router-dom"
 
 
-/*test('renders without crashing', () => {
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
-});*/
+});
 
 /*test('renders without crashing', () => {
   const div = document.createElement('div');
@@ -16,8 +18,8 @@ import App from './App';
 });*/
 
 
-test('renders learn react link', () => {
+/*test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     //API_ENDPOINT: 'http://localhost:8001'
-    API_ENDPOINT: 'https://murmuring-inlet-59810.herokuapp.com/'
+    API_ENDPOINT: 'https://salty-gorge-82559.herokuapp.com'
 }

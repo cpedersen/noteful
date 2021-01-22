b1aa28e91553d57b2ae33c64a103d92e5dfb9fe9
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-22 15:11:58 -0500

    Removing backup dir

--- src/BACKUP/notes-helpers.js
+++
@@ -1,7 +0,0 @@
-export const findFolder = (folders=[], folderId) =>
-  folders.find(folder => folder.id === folderId)
-
-export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
-
-//export const notes = notesContext.notes.filter(note => note.folderId === this.props.match.params.folder_id)

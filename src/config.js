19856a08e4af48fd525e2a0e4210ac0e7e0c72c0
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-04-27 19:36:27 -0400

    Repaired Note app

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 export default {
   // API_ENDPOINT: process.env.SERVER_ORIGIN,
   // API_ENDPOINT: "http://localhost:8001",
-  API_ENDPOINT: "https://salty-gorge-82559.herokuapp.com",
+  API_ENDPOINT:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8001"
+      : "https://salty-gorge-82559.herokuapp.com",
 };
